aeb79a0f6be6a5178fa8db70d87b5a8abb0d6f72
jhtdzhc12223 <126590718+jhtdzhc12223@users.noreply.github.com>
2025-03-26 08:24:50 -0300

    Add files via upload

---
+++ vs.code/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Olá Mundo\n');
+}).listen(3000);
+
+console.log('Servidor rodando em http://localhost:3000/');
+node server.js
