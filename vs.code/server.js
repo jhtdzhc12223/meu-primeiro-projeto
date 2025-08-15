dee4b41a850ead0c0c6204d4530acb169ead18f7
Alves <126590718+jhtdzhc12223@users.noreply.github.com>
2025-08-15 07:57:46 -0300

    Delete vs.code directory

--- vs.code/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Olá Mundo\n');
-}).listen(3000);
-
-console.log('Servidor rodando em http://localhost:3000/');
-node server.js
